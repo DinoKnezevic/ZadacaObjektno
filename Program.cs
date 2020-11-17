65d2837931af677713cf1c24e872bbc0b2a8d44c
DinoKnezevic <knezevic7p@gmail.com>
2020-11-17 19:59:20 +0100

    zavrseno

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using ClassLibrary1;
 using System;
 using ClassLibrary;
+using System.IO;
 
 namespace Zadaca1
 {
@@ -9,41 +10,49 @@ namespace Zadaca1
          
         static void Main(string[] args)
         {
-            Random rand = new Random();
-             double GenerateRandomScore()
-            {
-                double number;
-                number = rand.NextDouble() * 10.00;
-                return number;
-            }
+
             Description description = new Description(1, TimeSpan.FromMinutes(45), "Pilot");
             Console.WriteLine(description);
             Episode episode = new Episode(10, 88.64, 9.78, description);
             Console.WriteLine(episode);
             Console.WriteLine("!!!!!!!!!! ############# !!!!!!!!!!!!");
-            Episode ep1, ep2;
-            ep1 = new Episode();
-            ep2 = new Episode(10, 64.39, 8.7);
-            int viewers = 10;
-            //s
-            for (int i = 0; i < viewers; i++)
+
+            string fileName = @"C:\Users\Dino\source\repos\objektno\dz1\Zadaca1\shows.tv";
+            string[] episodesInputs = File.ReadAllLines(fileName);
+            Episode[] episodes = new Episode[episodesInputs.Length];
+            for (int i = 0; i < episodes.Length; i++)
             {
-                ep1.AddView(GenerateRandomScore());
-                Console.WriteLine(ep1.GetMaxScore());
+                episodes[i] = TvUtilities.Parse(episodesInputs[i]);
             }
-            if (ep1.GetAverageScore() > ep2.GetAverageScore())
-            {
-                Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
-            }
-            else
-            {
-                Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
-                
-            }
+            Console.WriteLine("Episodes:");
+            Console.WriteLine(string.Join<Episode>(Environment.NewLine, episodes));
+            TvUtilities.Sort(episodes);
+            Console.WriteLine("Sorted episodes:");
+            string sortedEpisodesOutput = string.Join<Episode>(Environment.NewLine, episodes);
+            Console.WriteLine(sortedEpisodesOutput);
+            //Episode ep1, ep2;
+            //ep1 = new Episode();
+            //ep2 = new Episode(10, 64.39, 8.7);
+            //int viewers = 10;
+            ////s
+            //for (int i = 0; i < viewers; i++)
+            //{
+            //    ep1.AddView(GenerateRandomScore());
+            //    Console.WriteLine(ep1.GetMaxScore());
+            //}
+            //if (ep1.GetAverageScore() > ep2.GetAverageScore())
+            //{
+            //    Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+
+            //}
+
+
         }
 
         
     }
 }
-
-// ep2 = new Episode(10, 64.39, 8.7); ovo sam riješio koristeći visual-ovu preporuku koristeći generate constructor
