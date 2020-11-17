04029caa66ac0d6340a7322c5141ab80c63783fe
DinoKnezevic <73589669+DinoKnezevic@users.noreply.github.com>
2020-11-17 20:02:50 +0100

    Add files via upload
    Zadaca2

---
+++ episode.cs
@@ -0,0 +1,96 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Episode
+    {
+        private int viewerNum=0;
+        private double sum = 0;
+        private double max = 0.00;
+
+        private Description description = new Description();
+
+        public Episode(int v, double s, double m)
+        {
+            this.viewerNum = v;
+            this.sum = s;
+            this.max = m;
+        }
+        public Episode(int v, double s, double m,Description description)
+        {
+            this.viewerNum = v;
+            this.sum = s;
+            this.max = m;
+
+            this.description.EpisodeNum = description.EpisodeNum;
+            this.description.EpisodeLength = description.EpisodeLength;
+            this.description.EpisodeName = description.EpisodeName;
+        }
+//s
+        public Episode()
+        {
+        }
+
+
+
+        //override ToString()
+        public override string ToString()
+        {
+            return $"{viewerNum},{sum},{max},{description.EpisodeNum},{description.EpisodeLength},{description.EpisodeName}";
+        }
+        //SETTERI
+        public int ViewerNum { get => viewerNum; set => viewerNum = value; }
+        public double Sum { get => sum; set => sum = value; }
+        public double Max { get => max; set => max = value; }
+        public Description Description { get => description; set => description=value; }
+
+        //END SETTERI
+        Random generate = new Random();
+
+        public void AddView(double randomScore)
+        {
+            sum += randomScore;
+            viewerNum++;
+            if(randomScore >max)
+            {
+                max = randomScore;
+            }
+        }
+        public double GetMaxScore()
+        {
+            return max;
+        }
+        public double GetAverageScore()
+        {
+            double average;
+            average = sum / viewerNum;
+            return average;
+        }
+        public int GetViewerCount()
+        {
+            return viewerNum;
+        }
+        //overload operatoraaa
+        public static bool operator >(Episode ep1,Episode ep2)
+        {
+            if(ep1.GetAverageScore()>ep2.GetAverageScore())
+            return true;
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <(Episode ep1,Episode ep2)
+        {
+            if (ep1.GetAverageScore() < ep2.GetAverageScore())
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ tvutilities.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using ClassLibrary1;
+
+namespace ClassLibrary
+{
+    public class TvUtilities
+    {
+        Random rand = new Random();
+        double GenerateRandomScore()
+        {
+            double number;
+            number = rand.NextDouble() * 10.00;
+            return number;
+        }
+
+        public static Episode Parse(string field)
+        {
+            Episode epizoda = new Episode();
+            Description opis = new Description();
+
+            string[] parts = field.Split(',');
+
+            epizoda.ViewerNum = Convert.ToInt32(parts[0]);
+
+            // stack overflow rjesenje start
+            // change the current culture to a culture that uses the dot as the decimal separator
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            //stack overflow rjesenje end
+
+            epizoda.Sum=double.Parse(parts[1]);
+            epizoda.Max = double.Parse(parts[2]);
+
+            opis.EpisodeNum = Convert.ToInt32(parts[3]);
+            opis.EpisodeLength = TimeSpan.Parse(parts[4]);
+            opis.EpisodeName = parts[5];
+            epizoda.Description = opis;
+
+            return epizoda;
+        }
+        public static void Sort(Episode[] episodes)
+        {
+            int i, j;
+            for(i=0;i<episodes.Length-1;i++)
+            {
+                for(j=0;j<episodes.Length-1-i;j++)
+                {
+                    if(episodes[j]<episodes[j+1])
+                    {
+                        Episode temp = new Episode();
+                        temp = episodes[j];
+                        episodes[j] = episodes[j + 1];
+                        episodes[j + 1] = temp;
+
+                    }
+                }
+            }
+        }
+    }
+    public class Description
+    {
+        private int episodeNum;
+        private TimeSpan episodeLength;
+        private string episodeName;
+
+        public TimeSpan EpisodeLength { get => episodeLength; set => episodeLength = value; }
+        public int EpisodeNum { get => episodeNum; set => episodeNum = value; }
+        public string EpisodeName { get => episodeName; set => episodeName = value; }
+
+        public Description(int episodeNum, TimeSpan episodeLength, string episodeName)
+        {
+            this.episodeNum = episodeNum;
+            this.episodeLength = episodeLength;
+            this.episodeName = episodeName;
+        }
+        public Description()
+        {
+
+        }
+        //s
+        public override string ToString()
+        {
+            return $"{episodeNum},{episodeLength},{episodeName}";
+        }
+    }
+ }
+
